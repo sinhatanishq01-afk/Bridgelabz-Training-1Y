7c42065d70f820c8f2932eec800e72845d410a08
Tanishq Sinha <sinhatanishq01@gmail.com>
2026-04-08 08:38:47 +0530

    program

---
+++ Java-leetcode/Lc151.java
@@ -0,0 +1,27 @@
+package com.gla.collection;
+
+
+    class Solution {
+        public String reverseWords(String s) {
+            List<String> words = new ArrayList<>();
+            int n = s.length();
+            for (int i = 0; i < n;) {
+                while (i < n && s.charAt(i) == ' ') {
+                    ++i;
+                }
+                if (i < n) {
+                    StringBuilder t = new StringBuilder();
+                    int j = i;
+                    while (j < n && s.charAt(j) != ' ') {
+                        t.append(s.charAt(j++));
+                    }
+                    words.add(t.toString());
+                    i = j;
+                }
+            }
+            Collections.reverse(words);
+            return String.join(" ", words);
+        }
+    }
+
+
